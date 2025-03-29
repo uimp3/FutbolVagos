6af5b54a8a9965aa125b6732883d00fe4edaa2af
Edwin <edwindanilo1110@udenar.edu.co>
2025-03-28 21:07:08 -0500

    factura y reservacion

--- FutbolVagos/core/admin.py
+++ FutbolVagos/core/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Cliente, Sede, Cancha, Trabajador, Horario
+from .models import Cliente, Sede, Cancha, Trabajador, Horario, Reservacion, Factura
 
 admin.site.register(Cliente)
 admin.site.register(Sede)
 admin.site.register(Cancha)
 admin.site.register(Trabajador)
 admin.site.register(Horario)
+admin.site.register(Reservacion)
+admin.site.register(Factura)
