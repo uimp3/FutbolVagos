2b91a5a20e42985c0c47cdb8363f4e00189b2e19
Edwin <edwindanilo1110@udenar.edu.co>
2025-03-28 21:02:50 -0500

    excepto factura y reservacion

--- FutbolVagos/FutbolVagos/wsgi.py
+++ FutbolVagos/FutbolVagos/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for futbolVagos project.
+WSGI config for FutbolVagos project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'futbolVagos.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FutbolVagos.settings')
 
 application = get_wsgi_application()
